0c54fee48d98ab44ab2e2341342452b7bec6be3e
Tom ten Thij <git@tomtenthij.nl>
2009-12-14 18:00:26 +0000

    Apply transforms to field values

--- lib/pickle/parser.rb
+++ lib/pickle/parser.rb
@@ -2,6 +2,9 @@ require 'pickle/parser/matchers'
 
 module Pickle
   class Parser
+    @@step_mother = nil
+    @@rb_language = nil
+
     include Matchers
     
     attr_reader :config
@@ -22,11 +25,52 @@ module Pickle
         raise ArgumentError, "The fields string is not in the correct format.\n\n'#{fields}' did not match: #{match_fields}" 
       end
     end
-    
+
+    def set_step_mother_from_object_space
+      return if @@step_mother
+      # This is getting ugly, I have not been able to find a  good way
+      # to get at the StepMother that is running the features from
+      # here. But ... we can fetch her from the object space.
+      ObjectSpace.each_object(Cucumber::StepMother) { |o| @@step_mother = o }
+      # There she is in all her glory. Can haz shower now?
+      @@step_mother = :undefined unless @@step_mother
+      @@step_mother
+    end
+
+    def set_rb_language
+      return if @@rb_language
+      set_step_mother_from_object_space
+
+      # This is getting ugly, I have not been able to find a  good way
+      # to get at the StepMother that is running the features from
+      # here. But ... we can fetch her from the object space.
+      ObjectSpace.each_object(Cucumber::StepMother) { |o| @@step_mother = o }
+      # There she is in all her glory. Now I feel like I need a shower
+
+      @@rb_language = if @@step_mother == :undefined
+                        :undefined
+                      else
+                        @@step_mother.load_programming_language('rb')
+                      end
+    end
+
+    def execute_transforms(arg)
+      set_rb_language
+      if @@rb_language == :undefined
+        arg
+      else
+        @@rb_language.execute_transforms([arg]).first
+      end
+    end
+
     # given a string like 'foo: expr' returns {key => value}
     def parse_field(field)
       if field =~ /^#{capture_key_and_value_in_field}$/
-        { $1 => eval($2) }
+        value = eval($2)
+        if value.respond_to?(:match)
+          value = execute_transforms(value)
+        end
+        { $1 => value }
       else
         raise ArgumentError, "The field argument is not in the correct format.\n\n'#{field}' did not match: #{match_field}"
       end
